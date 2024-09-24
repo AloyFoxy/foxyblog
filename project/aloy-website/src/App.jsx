a27376814d8411cf7f83acb176894cd5ee7758e4
AloyFoxy <aloyfowoxy@gmail.com>
2024-09-24 10:57:41 -0300

    checkpoint con callous conjunction

---
+++ project/aloy-website/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <section className="flex flex-col justify-center items-center w-full bg-gray-100">
+      <div className="flex flex-col items-center bg-gray-500 p-4 rounded w-3/4">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <p>Esta es la historia de mi vida y obra maestra</p>
+      </div>
+      <div>
+        <p>
+          Aquí crearé mi sitio web bien bomnito 💜
+        </p>
+      </div>
+    </section>
+  )
+ }
